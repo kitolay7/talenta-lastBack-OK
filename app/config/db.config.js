dc438d1df41cc6d1a4bc8b768c1062685340b187
manolotsoo <manolotsoadaniel@gmail.com>
2020-10-05 18:06:23 +0000

    add database's configuration

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   // HOST: "127.4.188.2",
-  HOST: "154.126.92.194",
-  USER: "root",
-  PASSWORD: "",
-  DB: "testdb",
+  HOST: "remotemysql.com",
+  USER: "rtiQxD4A8F",
+  PASSWORD: "TJyPqrM399",
+  DB: "rtiQxD4A8F",
   dialect: "mysql",
+  PORT: "3306",
   pool: {
     max: 5,
     min: 0,
